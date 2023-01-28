7660678813fcba24a1ef2de4300d29a3f82b3895
Basirul Billah <basirulbillah@gmail.com>
2023-01-28 20:27:37 +0600

    added appointment hero  section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,22 +1,28 @@
 import React from "react";
 import chair from "../../../assets/images/chair.png";
+import bg from "../../../assets/images/bg.png";
+import PrimaryButton from "../../../Components/PrimaryButton";
 
 const Banner = () => {
   return (
-    <div>
+    <div
+      style={{
+        background: `url(${bg})`,
+      }}
+    >
       <div className="hero text-accent">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} className="rounded-lg lg:w-1/2 shadow-xl" alt="" />
           <div>
-            <h1 className="text-2xl lg:text-5xl font-bold">Smile with Confidence!</h1>
+            <h1 className="text-2xl lg:text-5xl font-bold">
+              Smile with Confidence!
+            </h1>
             <p className="py-6">
               Experience exceptional dental care with our highly skilled team.
               From routine checkups to advanced treatments, we provide
               comprehensive care for all ages.
             </p>
-            <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
-              Get Started
-            </button>
+            <PrimaryButton>Get Started</PrimaryButton>
           </div>
         </div>
       </div>
---
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,42 @@
+import React from "react";
+import doctor from "../../../assets/images/doctor.png";
+import appointment from "../../../assets/images/appointment.png";
+import PrimaryButton from "../../../Components/PrimaryButton";
+
+const MakeAppointment = () => {
+  return (
+    <section className="mt-32">
+      <div
+        className="hero bg-base-200"
+        style={{
+          background: `url(${appointment})`,
+        }}
+      >
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={doctor} className="-mt-32 hidden md:block lg:w-1/2" alt="" />
+          <div>
+            <h4 className="uppercase text-lg text-primary font-bold">
+              appointment
+            </h4>
+            <h1 className="text-4xl font-bold text-white">
+              Make an appointment today!
+            </h1>
+            <p className="py-6 text-justify text-white">
+              Welcome to our state-of-the-art medical facility, where our team
+              of highly skilled and compassionate doctors are dedicated to
+              providing the highest level of care to our patients. With our
+              online appointment scheduling system, it's never been easier to
+              book an appointment with one of our specialists. Whether you're in
+              need of a routine check-up or a specialized treatment, our doctors
+              are here to help. Trust us to be your partner in health and
+              wellness, and book your appointment today!
+            </p>
+            <PrimaryButton>make an appointment</PrimaryButton>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MakeAppointment;
